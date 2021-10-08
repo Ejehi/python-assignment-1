e230a2989dcb956f4b97aed6d5cfa995d837e629
ejehi <ijezry@gmail.com>
2021-10-08 16:08:21 +0100

    second commit

--- all_even_numbers.py
+++ all_even_numbers.py
@@ -1,42 +1,33 @@
-your_list = []
+valid_integer_list = []
 
 def check_if_integer(any_number):
     try:
         return int(any_number)
     except ValueError:
-        print('You did not enter an integer. Please try again')
         return False
 
-def check_even_number():
-    length_of_list = input("Please enter the number of integers you want in your list")
+def collect_user_list():
+    user_input = input("Please enter all the numbers you want in your list seperated by a whitespace")
+    user_list = user_input.split(" ")
+    for item in user_list:
+        check_item = check_if_integer(item)
+        valid_integer_list.append(check_item)
 
-    valid_length_of_list = check_if_integer(length_of_list)
+collect_user_list()
 
-    if valid_length_of_list:
+while False in valid_integer_list:
+    print("Please enter only integer values. Try again")
+    valid_integer_list.clear()
+    collect_user_list()
 
-        # adding the user's list elements
-        for number_input in range(valid_length_of_list):
-            list_element = input("Enter an integer list item")
-            valid_list_element = check_if_integer(list_element)
+# check to see if numbers are even
+even_number_list = []
+for item in valid_integer_list:
+    if item % 2 == 0:
+        even_number_list.append(item)
 
-            while not isinstance(valid_list_element, int):
-                list_element = input("Enter an integer list item")
-                valid_list_element = check_if_integer(list_element)
-
-            your_list.append(valid_list_element) 
-
-     
-
-        # check to see if numbers are even
-        even_number_list = []
-        for item in your_list:
-            if item % 2 == 0:
-                even_number_list.append(item)
-
-        if even_number_list == []:
-            print("There are no even numbers contained in your list")
-        else:
-            print("The list below contains all even numbers in your list:")
-            print(even_number_list)
-
-check_even_number()
+if even_number_list == []:
+    print("There are no even numbers contained in your list")
+else:
+    print("The list below contains all even numbers in your list:")
+    print(even_number_list)
--- difference_between_two_dates.py
+++ difference_between_two_dates.py
@@ -6,15 +6,19 @@ def validate_date_format(date_string):
          
     except ValueError:
         return False
-first_string = str(input('Enter date(yyyy-mm-dd): '))
-second_string = str(input('Enter date(yyyy-mm-dd): '))
-date1 = validate_date_format(first_string)
-date2 = validate_date_format(second_string)
+
+first_date_string = str(input('Enter date(yyyy-mm-dd):'))
+second_date_string = str(input('Enter another date(yyyy-mm-dd):'))
+date1 = validate_date_format(first_date_string)
+date2 = validate_date_format(second_date_string)
 
 if date1 == False and date2 == False:
-    print("Incorrect data format, should be YYYY-MM-DD")
-    print("Try again")
+    print("Incorrect data format, should be YYYY-MM-DD. Try again")
 else:
-    date_difference = date2 - date1
-    print(date_difference)
+    if date1 < date2:
+        date_difference = date2 - date1
+        print(date_difference)
+    else:
+        date_difference = date1 - date2
+        print(date_difference)
 
